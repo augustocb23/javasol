9d53d05dfc192a93b4cd4277a08cc30f80e1347f
Augusto César Bisognin <augustocb@gmail.com>
2019-11-17 01:13:45 -0300

    refactor Solitaire (1)

--- src/com/fbergeron/card/Value.java
+++ src/com/fbergeron/card/Value.java
@@ -38,14 +38,14 @@ public class Value {
     public static final Value V_8 = new Value(8);
     public static final Value V_10 = new Value(10);
     public static final Value V_9 = new Value(9);
-    public static final Value V_QUEEN = V_12;
-    public static final Value V_KING = V_13;
     private static final Value V_11 = new Value(11);
     public static final Value V_JACK = V_11;
     private static final Value V_12 = new Value(12);
+    public static final Value V_QUEEN = V_12;
     private static final Value V_1 = new Value(1);
-    private static final Value V_13 = new Value(13);
     public static final Value V_ACE = V_1;
+    private static final Value V_13 = new Value(13);
+    public static final Value V_KING = V_13;
     static final Value[] values = {
             V_1, V_2, V_3, V_4, V_5,
             V_6, V_7, V_8, V_9, V_10,
